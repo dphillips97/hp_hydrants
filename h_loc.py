26c8c6dd4528f99830995fcd1f616d8732deed8b
dphillips97 <dphillips97@gmail.com>
2018-03-15 15:38:27 -0400

    1.0

--- h_loc.py
+++ h_loc.py
@@ -3,44 +3,73 @@ import requests
 import json
 from api_keys import *
 
-URL = r'https://maps.googleapis.com/maps/api/geocode/json?'
-API_KEY = GITHUB_API_KEY
+def geocode(loc):	
 
+	API_KEY = GITHUB_API_KEY
+	
+	URL = r'https://maps.googleapis.com/maps/api/geocode/json?'
+	
+	city = ',Hazel_Park'
+	state = ',MI'
 
-
-
-def read_wb():
-
+	unique_url = URL + 'address=%s&key=%s' % ((loc + city + state), API_KEY)
+	
+	results = requests.get(unique_url)
+	data = json.loads(results.text)
+		
+	if (len(data['results'])) != 0:
+		
+		print('%s - FOUND' % loc)
+		
+		lat = data['results'][0]['geometry']['location']['lat']
+		lng = data['results'][0]['geometry']['location']['lng']
+		
+		# list only takes 1 argument
+		results_tuple = (lat, lng)
+		results = list(results_tuple)
+	
+		return results
+	
+	else:
+		
+		print('%s - NOT FOUND' % loc)
+		return
+	
+def main():
+	
 	# open sheet object
 	wb = openpyxl.load_workbook('hydrant_loc.xlsx')
 	sheet = wb.active
 	
-	max_rows = sheet.max_row
+	# get max row and col for later use
+	max_row = sheet.max_row
+	max_col = sheet.max_column
 	
-	# pass sheet object and max rows to main()
-	return sheet, max_rows
-
-def geocode(loc, URL):	
-
-	city = ',Hazel_Parl'
-	state = ',MI'
-
-	unique_url = URL + 'address=%s&key=%s' % ((loc + city + state), API_KEY)
-	results = requests.get(unique_url)
+	# for every record after headers
+	for row_iter in range(2, max_row + 1):
 	
-	data = json.loads(results.txt)
-	
-	
-	
-def main():
-	
-	# get sheet object and max rows from other function
-	sheet, max_rows = read_wb()
-	
-	for row_iter in range(2, max_rows):
-		
+		# get location
 		loc = sheet.cell(row = row_iter, column = 2).value
-	
+		
+		# call geocode function and return list of lat, long
+		results = geocode(loc)
+		
+		# results = [lat, lng]
+		if results:
+		
+			# unpack list
+			lat = results[0]
+			lng = results[1]
+		
+			# write values
+			sheet.cell(row = row_iter, column = max_col + 1).value = lat
+			sheet.cell(row = row_iter, column = max_col + 2).value = lng
+			
+		else:
+		
+			pass
+		
+	wb.save('hydrants_coded.xlsx')
 	
 		
 main()
